132063e7d200d903ab79b1aeb6a7cebaca95de7c
xRepZ <xzbnyby@yahoo.com>
2023-09-11 01:44:56 +0300

    port change

--- src/api/ws.js
+++ src/api/ws.js
@@ -1,36 +1,8 @@
-// export const wsConnection = new WebSocket("ws://todo.my/api/ws");
-// wsConnection.onopen = () => {
-//     alert("Соединение установлено.");
-// }
-
-// wsConnection.onclose = (event) => {
-//     if (event.wasClean) {
-//         alert('Соединение закрыто чисто');
-//     } else {
-//         alert('Обрыв соединения'); // например, "убит" процесс сервера
-//     }
-//     alert('Код: ' + event.code + ' причина: ' + event.reason);
-// }
-
-// wsConnection.onerror = (error) => {
-//     alert("Ошибка " + error.message);
-// }
-
-// export const wsSend = (data) => {
-// // readyState - true, если есть подключение
-//     if(!wsConnection.readyState){
-//         setTimeout(function (){
-//             wsSend(data);
-//         },100);
-//     } else {
-//         wsConnection.send(data);
-//     }
-// }
 
 export default {
     async connect() {
         const ws = await new Promise((resolve, reject) => {
-            const ws = new WebSocket("ws://todo.my/api/ws")
+            const ws = new WebSocket("wss://todolistwss.freemyip.com/api/ws")
             // const ws = new WebSocket("ws://localhost:5173/api/ws")
             ws.onopen = () => {
                 ws.send(JSON.stringify({
